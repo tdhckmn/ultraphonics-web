123c0f715f0658dd58ef0941f6fe0a4f88cad9b4
Tom Hickman <tomh25@protonmail.com>
2026-01-03 14:32:50 -0500

    phone install

---
+++ admin/sw.js
@@ -0,0 +1,42 @@
+// Simple service worker for PWA functionality
+const CACHE_NAME = 'ultraphonics-admin-v1';
+
+// Install event - cache key assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/admin/index.html',
+        '/admin/manifest.json',
+        '/assets/images/logo-color.png',
+        '/assets/images/favicon.ico'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch event - serve from cache, fallback to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
